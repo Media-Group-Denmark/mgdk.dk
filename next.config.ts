e929237ddf2d77873dcb412e1731e306a6fb9f3c
Frederik Graakjær <frede.flip@gmail.com>
2025-11-06 10:44:48 +0100

    feat: add image remote patterns for WordPress uploads

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "newsflow.mgdk.dk",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
