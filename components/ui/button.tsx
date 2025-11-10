05a23418ca1a052cf3d6b4d4af16c85f5a747ec9
Frederik Graakjær <frede.flip@gmail.com>
2025-11-10 14:49:37 +0100

    added md size button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full font-normal transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-full font-inter font-normal transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
       variant: {
@@ -26,7 +26,8 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3 text-md",
         sm: "h-8 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5 text-sm",
-        lg: "max-h-13 py-3 rounded-full px-7 has-[>svg]:px-4 text-[12px] md:text-[14px] lg:text-[16px]",
+        md: "max-h-13 min-w-[135px] py-3 rounded-full px-7 has-[>svg]:px-4 text-[12px] md:text-[14px] lg:text-[16px]",
+        lg: "max-h-13 min-w-[150px] py-3 rounded-full px-7 has-[>svg]:px-4 text-[15px] md:text-[16px] lg:text-[18px]",
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
