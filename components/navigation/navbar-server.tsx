55da82914e680def76fea8144e37d16775c55f90
Frederik Graakjær <frede.flip@gmail.com>
2025-11-13 10:18:51 +0100

    added seperate function to fecth menus (footer and navbar)

--- components/navigation/navbar-server.tsx
+++ components/navigation/navbar-server.tsx
@@ -1,35 +1,7 @@
-import { wp } from "@/lib/wp-server";
-import Navbar from "./Navbar";
-
-type Item = {
-  id: number;
-  title: { rendered: string };
-  url: string;
-  parent: number;
-  menu_order: number;
-};
-
-function toPublicPath(inputUrl: string): string {
-  const match = inputUrl.match(/[^\/]+(?=\/?$)/);
-  return match ? `/${match[0]}` : "/";
-}
-
-async function getMenu(): Promise<Item[]> {
-  const loc = await wp("/wp/v2/menu-locations/primary?context=edit");
-  const items: Item[] = await wp(
-    `/wp/v2/menu-items?menus=${loc.menu}&per_page=100&_fields=id,title,url,parent,menu_order&context=edit`,
-    { next: { revalidate: 60 } }
-  );
-  const normalized = items
-    .map((i) => ({
-      ...i,
-      url: toPublicPath(i.url),
-    }))
-    .sort((a, b) => a.menu_order - b.menu_order);
-  return normalized;
-}
+import { getMenu } from "@/lib/getMenu";
+import { Navbar } from "./Navbar";
 
 export default async function NavbarServer() {
-  const items = await getMenu();
+  const items = await getMenu("primary");
   return <Navbar items={items} />;
 }
